c5acd010c737b8e2c72f73943cde00a45f163967
suci <suciwidhi22@gmail.com>
2023-05-19 00:11:18 +0800

    page link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../images/logo.svg';
-import { pageLinks, socialLinks } from '../data';
+import { socialLinks } from '../data';
+import PageLinks from './PageLinks';
 
 const NavBar = () => {
     return (
@@ -14,18 +15,8 @@ const NavBar = () => {
                         </button>
                     </div>
                     { /* <!-- left this comment on purpose --> */}
-                    <ul className="nav-links" id="nav-links">
-                        {pageLinks.map((link) => {
-                            const { id, href, text } = link;
-                            return (
-                                <li key={id}>
-                                    <a href={href}
-                                        className="nav-link">
-                                        {text}</a>
-                                </li>
-                            )
-                        })}
-                    </ul>
+                    <PageLinks parentClass='nav-links' itemClass='nav-link' />
+
 
                     <ul className="nav-icons">
                         {socialLinks.map((social) => {
